8f2bbbe92838948728581ec4240d691107e18230
BuildTools <unconfigured@null.spigotmc.org>
2022-10-06 18:41:21 -0400

    added checks for roles

--- react-app/src/components/DummyLanding.tsx
+++ react-app/src/components/DummyLanding.tsx
@@ -1,8 +1,22 @@
 import React, {useState} from 'react'
-import {getAuth} from 'firebase/auth'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 const DummyLanding = () => {
 
     const [role, setRole] = useState<String | null>(null);
+    const firebaseConfig = {
+        apiKey: "AIzaSyAy9GLcZ03dfe31fGK2vxmmCgFno8X3Hpg",
+        authDomain: "rise-dc-fd5a6.firebaseapp.com",
+        projectId: "rise-dc-fd5a6",
+        storageBucket: "rise-dc-fd5a6.appspot.com",
+        messagingSenderId: "650118133496",
+        appId: "1:650118133496:web:e3dca21534a8947100ad0d",
+        measurementId: "G-CRHN8KVGQK"
+    };
+    const app = initializeApp(firebaseConfig);
     const auth = getAuth()
     const user = auth.currentUser
     if (user) {
@@ -21,7 +35,7 @@ const DummyLanding = () => {
 
     return (
             <div>
-                {role}
+                {user ? role : "You cannot access this page."}
             </div>
     )
 }
--- react-app/src/components/DummyLogin.tsx
+++ react-app/src/components/DummyLogin.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 //import app from '../config/firebase'
+import { useNavigate } from 'react-router-dom';
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
@@ -7,6 +8,7 @@ import {getStorage} from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const DummyLogin = () => {
+    const navigate = useNavigate();
     const firebaseConfig = {
         apiKey: "AIzaSyAy9GLcZ03dfe31fGK2vxmmCgFno8X3Hpg",
         authDomain: "rise-dc-fd5a6.firebaseapp.com",
@@ -17,20 +19,27 @@ const DummyLogin = () => {
         measurementId: "G-CRHN8KVGQK"
     };
     const app = initializeApp(firebaseConfig);
+    const auth = getAuth();
+    const user = auth.currentUser
+    // if (user) {
+    //     navigate("../landing")
+    // }
 
     const [loginInfo, setLoginInfo] = useState({email: '', password: ''})
-    const auth = getAuth();
+    const [error, setError] = useState<Boolean | null>(null);
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         signInWithEmailAndPassword(auth, loginInfo.email, loginInfo.password)
             .then((userCredential) => {
                 const user = userCredential.user;
+                navigate("../landing")
                 console.log(user)
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                setError(true)
             });
         
     }    
@@ -40,6 +49,13 @@ const DummyLogin = () => {
             <input id="usernameInput" placeholder = 'email' type="text" onChange = {(e) => setLoginInfo ({...loginInfo, email: e.target.value})}/>
             <input id="usernameInput" placeholder = 'password' type="text" onChange = {(e) => setLoginInfo ({...loginInfo, password: e.target.value})}/>
             <button type="submit">Submit</button>
+            {error ? 
+                <div>
+                    <br/>
+                    "Incorrect Username or Password"
+                </div> 
+                : <div></div>
+            }
         </form>
     )
 }
