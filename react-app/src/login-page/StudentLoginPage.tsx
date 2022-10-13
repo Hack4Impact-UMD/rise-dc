488924807b57328382321593397e6da813fea7eb
Wenjie <wwei4949@gmail.com>
2022-10-13 16:17:43 -0400

    added logo and adjusted text field size to login page

--- react-app/src/login-page/StudentLoginPage.tsx
+++ react-app/src/login-page/StudentLoginPage.tsx
@@ -1,37 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthError, getAuth } from "@firebase/auth";
-import TextField, {TextFieldTypes} from "./TextField/TextField"
+import TextField, { TextFieldTypes } from "./TextField/TextField"
 import Button from "./Button/Button";
-
+import logo from "./assets/rise-dc-logo.png";
 import "./LoginPage.css";
 import app from "../config/firebase";
 
 const StudentLoginPage: React.FC<any> = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
 
-    return (
+  return (
     <div className="login-page">
-      <div className="login-fields">
-        <TextField 
-        header = "Email"
-        isDisabled={isLoading}
-        fieldType={TextFieldTypes.email}
-        onChange={(val) => setEmail(val)}/>
+      <div className="login-card">
+        <img id="login-logo" src={logo} />
+        <TextField
+          header="Email"
+          isDisabled={isLoading}
+          fieldType={TextFieldTypes.email}
+          onChange={(val) => setEmail(val)} />
         <a href="" className="forgot">Forgot Email?</a>
-        <TextField 
-        header = "Password"
-        isDisabled={isLoading}
-        fieldType={TextFieldTypes.password}
-        onChange={(val) => setPassword(val)}/>
+        <TextField
+          header="Password"
+          isDisabled={isLoading}
+          fieldType={TextFieldTypes.password}
+          onChange={(val) => setPassword(val)} />
         <a href="" className="forgot">Forgot Password?</a>
-        <Button text="Login" isDisabled={isLoading}/>
+        <Button text="Login" isDisabled={isLoading} />
       </div>
-      
+
     </div>
   );
 };
