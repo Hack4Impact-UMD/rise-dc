35863bf7d635bd946dd2f72af233bee91057d485
Wenjie <wwei4949@gmail.com>
2022-10-20 09:31:44 -0400

    adjusted format

--- react-app/src/components/Navbar.tsx
+++ react-app/src/components/Navbar.tsx
@@ -7,11 +7,9 @@ import setting from "./assets/setting.png";
 const NavBar = () => {
   return (
     <nav className = "navbar">
-      <div className="buttons">
         <NavLink className="nav" to="/"> <img src={home} alt="Home" /> </NavLink>
         <NavLink className="nav" id="log" to="/log"> <img src={log} alt="Log" /></NavLink>
         <NavLink className="nav" id="settings" to="/settings"> <img src={setting} alt="Settings" /></NavLink>
-      </div>   
     </nav>
   );
 }
