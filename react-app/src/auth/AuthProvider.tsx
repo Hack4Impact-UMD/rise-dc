15b29a726ec707d2ba38668c86ce02bd391573e7
andrew <40076917+a-ys@users.noreply.github.com>
2022-11-05 05:32:31 -0400

    Add loading state to AuthProvider. Implement ... authentication of admin settings page.

--- react-app/src/auth/AuthProvider.tsx
+++ react-app/src/auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { getAuth, onIdTokenChanged } from '@firebase/auth';
+import { getAuth, onIdTokenChanged, User, IdTokenResult } from '@firebase/auth';
 import React, { createContext, useContext, useEffect, useState} from 'react';
 import app from '../config/firebase';
 
@@ -8,36 +8,37 @@ interface Props {
 }
 
 interface AuthContextType {
-  user: any;
-  token: IdToken;
+  user: User;
+  token: IdTokenResult;
+  loading: boolean;
 }
 
-interface IdToken {
-  claims: {
-    role: String
-  }
-}
-
+// The AuthContext that other components may subscribe to.
 let AuthContext = createContext<AuthContextType>(null!);
 
-
+// Updates the AuthContext and re-renders children when the user changes.
+// See onIdTokenChanged for what events trigger a change.
 export const AuthProvider: React.FC<Props> = ({children}) => {
-  const [user, setUser] = useState<any>({});
-  const [token, setToken] = useState<any>({claims: {role: "none"}});
+  const [user, setUser] = useState<User | any>(null!);
+  const [token, setToken] = useState<IdTokenResult>(null!);
+  // The loading state is used by RequireAuth/RequireAdminAuth
+  const [loading, setLoading] = useState<boolean>(true);
+
   useEffect(() => {
     const auth = getAuth(app);
-    onIdTokenChanged(auth, (user) => {
-      setUser(user);
-      if (user != null) {
-        user.getIdTokenResult().then((token) => {
-          setToken(token);
+    onIdTokenChanged(auth, (newUser) => {
+      setUser(newUser);
+      if (newUser != null) {
+        newUser.getIdTokenResult().then((newToken) => {
+            setToken(newToken);
         });
       }
+      setLoading(false);
     });
   }, [])
 
   return (
-    <AuthContext.Provider value={{user, token }}>
+    <AuthContext.Provider value={{user,token, loading}}>
       {children}
     </AuthContext.Provider>
   )
