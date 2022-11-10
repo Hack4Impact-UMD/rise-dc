2917dc40d5e89ce0ca646c4ba2b494c6c121334e
andrew <40076917+a-ys@users.noreply.github.com>
2022-11-10 14:14:27 -0500

    Search Page fixes: imports, remove button comp... Also updated formatting

--- react-app/src/search-page/SearchPage.tsx
+++ react-app/src/search-page/SearchPage.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import "./SearchPage.css"
-import FormInput from '../settings-page/FormInput';
-import AddElementButton from './AddElementButton';
 import StudentProfile from './StudentProfile';
-import notepad from './assets/notepad.svg';
-import person from './assets/person.svg';
 
 export default function SearchPage() {
     return (
-    <div className='container'>
+    <div className="container">
         <div className = "header"></div> {/* Inset Navbar Here */}
-        <div className='body'>
-            <div className='green-background'>
-                <div className = 'white-background'>
-                    <div className='display-horizontal'>
-                        <input className = 'search' name = 'email' placeholder = "Search"/>
-                        <AddElementButton className = 'element-button' text='New Student' />
+        <div className="body">
+            <div className="green-background">
+                <div className="white-background">
+"                   <div className="display-horizontal">
+                        <input className="search" name="email" placeholder="Search"/>
+                        <button className="element-button">New Student</button>
                     </div>
                     <StudentProfile/>
                 </div>
