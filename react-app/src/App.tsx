67a7afd94989e029b45c5969c506e927998f4fbb
Sahil Gaba <sgaba@terpmail.umd.edu>
2022-11-10 11:56:40 -0500

    Fixed merge conflicts in app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-import RequireAuth from './auth/RequireAuth';
-import DummyLogin from './temp-components/DummyLogin'
-import Landing from './landing-page-components/Landing';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RequireAuth from "./auth/RequireAuth";
+import Login from "./login-page/Login";
+import RequireAdminAuth from "./auth/RequireAdminAuth";
+import Landing from "./landing-page-components/Landing";
+import AdminSettingsPage from "./settings-page/AdminSettingsPage";
 import "./App.css";
-import { AuthProvider } from './auth/AuthProvider';
-import Session from './session-log-components/Session';
-
+import { AuthProvider } from "./auth/AuthProvider";
+import Session from "./session-log-components/Session";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path = '/' element = {<DummyLogin/>} />
-          <Route path = "/login" element = {<DummyLogin/>} /> 
-          <Route path = "/landing" element = {<RequireAuth children = {<Landing/>} />} />
-          <Route path = "/logs" element = {<RequireAuth children = {<Session/>} />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/landing"
+            element={<RequireAuth children={<Landing />} />}
+          />
+          <Route
+            path="/settings"
+            element={<RequireAdminAuth children={<AdminSettingsPage />} />}
+          />
+          <Route
+            path="/logs"
+            element={<RequireAuth children={<Session />} />}
+          />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
