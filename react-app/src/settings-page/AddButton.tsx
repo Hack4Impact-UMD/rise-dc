d12b99e4943064d27144d0214b58410cc54af351
andrew <40076917+a-ys@users.noreply.github.com>
2022-11-14 18:13:43 -0500

    Settings page user creation backend functionality

--- react-app/src/settings-page/AddButton.tsx
+++ react-app/src/settings-page/AddButton.tsx
@@ -1,5 +1,9 @@
 import styles from "./AdminSettingsPage.module.css";
 
-export default function ResetButton() {
-  return <button className={styles.resetButton}>Add</button>;
+export default function ResetButton(props: any) {
+  return (
+    <button className={styles.resetButton} type="submit" name={props.name}>
+            Add
+    </button>
+  );
 }
