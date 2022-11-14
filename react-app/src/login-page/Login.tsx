e8e7aad2c31f35be140bca96fdf9a917c925b56e
Jacob Lane <jacobdlane2002@gmail.com>
2022-11-13 21:01:26 -0500

    Adding test students (FOR TESTING ONY)

--- react-app/src/login-page/Login.tsx
+++ react-app/src/login-page/Login.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { authenticateUser } from "../backend/FirebaseCalls";
 import TextField, { TextFieldTypes } from "./TextField/TextField";
 import Button from "./Button/Button";
 import logo from "./assets/rise-dc-logo.png";
 import styles from "./Login.module.css";
+import "./Login.css";
+import app from "../config/firebase";
+import {authenticateUser} from "../backend/FirebaseCalls";
+import { addStudent, addLog } from "../backend/FirestoreCalls";
 
 const LoginPage: React.FC<any> = () => {
   const [email, setEmail] = useState<string>("");
@@ -14,6 +17,38 @@ const LoginPage: React.FC<any> = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const navigate = useNavigate();
+
+  const addDummyUsers = () => {
+    for(let i = 0; i < 5; i++)
+      try{
+      addStudent({
+        address: "address1",
+        email: i+"@e.mail",
+        grade_level: i+"",
+        grades: {
+          english_before: "A",
+          english_after: "A",
+          humanities_before: "A",
+          humanities_after: "A",
+          socialStudies_before: "A",
+          socialStudies_after: "A",
+          math_before: "A",
+          math_after: "A",
+          science_before: "A",
+          science_after: "A"
+        },
+        guardian_email: i+"@e.mail",
+        guardian_name: "guardian "+i,
+        guardian_phone: "000-000-000"+i,
+        high_school: "school"+i,
+        name: "name "+i,
+        phone_number: "000-000-000"+i,
+        reading_level: ""+i
+      });
+    } catch {
+      console.log("error adding student")
+    }
+  }
 
   const login = () => {
     authenticateUser(email, password)
