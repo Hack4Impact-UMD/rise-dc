26a4d253fd9e12e7f0729a9250f54e1bdd7379a8
lmuratore <ltm157m@gmail.com>
2022-11-19 17:56:02 -0500

    Removed edit/save button in upload form section

--- react-app/src/student-profile/Forms/Forms.tsx
+++ react-app/src/student-profile/Forms/Forms.tsx
@@ -3,23 +3,13 @@ import styles from "./Forms.module.css";
 import symbol from "./fileUpload.svg";
 
 const Forms = () => {
-  const [edit, setEdit] = useState<boolean>(false);
 
   const [files, setFiles] = useState<any[]>([]);
-
-  const handleEdit = (event: React.MouseEvent<HTMLElement>) => {
-    setEdit(!edit);
-  };
 
   return (
     <div className={styles.studentSession}>
       <div className={styles.topLine}>
         <h2 className={styles.title}>Forms</h2>
-        <div className={styles.editButtons}>
-          <button className={styles.edit} onClick={handleEdit}>
-            {edit ? "Save" : "Edit"}
-          </button>
-        </div>
       </div>
       <div className={styles.container}>
         <div className={`${styles.containerLines} ${styles.uploadLine}`}>
@@ -29,18 +19,14 @@ const Forms = () => {
             onChange={(e: any) => setFiles([...files, e!.target!.files[0]])}
             hidden
           />
-          {edit ? (
-            <label htmlFor="upload" className={styles.edit}>
-              Upload{" "}
-              <img
-                src={symbol}
-                alt="File Upload Symbol"
-                className={styles.uploadImage}
-              ></img>
-            </label>
-          ) : (
-            <></>
-          )}
+          <label htmlFor="upload" className={styles.edit}>
+            Upload{" "}
+            <img
+              src={symbol}
+              alt="File Upload Symbol"
+              className={styles.uploadImage}
+            ></img>
+          </label>
         </div>
         {files.map((file) => {
           return (
