bf2b35119b0f30c869081e945cd647b93e869595
lmuratore <ltm157m@gmail.com>
2022-11-28 19:09:41 -0500

    Removed duplicate identifier in LogType

--- react-app/src/types/LogType.tsx
+++ react-app/src/types/LogType.tsx
@@ -3,7 +3,6 @@ export type Log = {
     duration_minutes : number,
     instructor_name : string,
     reason : string,
-    student_id : string,
     creator_id : string,
     subject : Subject,
     summary : string,
