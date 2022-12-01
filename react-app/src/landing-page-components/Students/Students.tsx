8d6e1df05c3663495f98346cf0f8278e6990dc1b
Jacob Lane <jacobdlane2002@gmail.com>
2022-12-01 11:03:00 -0500

    Connected backend to landing page

--- react-app/src/landing-page-components/Students/Students.tsx
+++ react-app/src/landing-page-components/Students/Students.tsx
@@ -1,54 +1,44 @@
 import person from "./person.svg";
 import notepad from "./notepad.svg";
 import styles from "./Students.module.css";
+import {Student} from "../../types/StudentType"
+import { useNavigate } from "react-router-dom";
 
-const Students = ({ props }: any) => {
-  const handleClickPlus = () => {
-    console.log("+ icon");
+interface Props {
+  students: Student[]
+  allStudentsLink: string
+}
+
+const Students = ({students, allStudentsLink}: Props) => {
+  const navigate = useNavigate();
+  const handleClickProfile = (student: Student) => {
+    navigate(`/profile/${student.id}`)
   };
-  const handleClickSettings = () => {
-    console.log("settings icon");
+  const handleClickLog = () => {
+    navigate("/log")
   };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}> Students </h1>
-      <div className={styles.student}>
-        <h1 className={styles.studentName}> Alice Lee</h1>
+      <>
+      {students.map(student => (
+        <div className={styles.student} key={student.name}>
+        <h1 className={styles.studentName}>{student.name}</h1>
         <div className={styles.icons}>
-          <button onClick={handleClickSettings} className={styles.button}>
+          <button onClick={handleClickLog} className={styles.button}>
             <img src={notepad} className={styles.icon} alt="Notepad Icon" />
           </button>
-          <button onClick={handleClickPlus} className={styles.button}>
+          <button onClick={() => {handleClickProfile(student)}} className={styles.button}>
             <img src={person} className={styles.icon} alt="Person Icon" />
           </button>
         </div>
       </div>
-      <div className={styles.student}>
-        <h1 className={styles.studentName}> Bobby Clark </h1>
-        <div className={styles.icons}>
-          <button onClick={handleClickSettings} className={styles.button}>
-            <img src={notepad} className={styles.icon} alt="Notepad Icon" />
-          </button>
-          <button onClick={handleClickPlus} className={styles.button}>
-            <img src={person} className={styles.icon} alt="Person Icon" />
-          </button>
-        </div>
-      </div>
-      <div className={styles.student}>
-        <h1 className={styles.studentName}> Eve Smith</h1>
-        <div className={styles.icons}>
-          <button onClick={handleClickSettings} className={styles.button}>
-            <img src={notepad} className={styles.icon} alt="Notepad Icon" />
-          </button>
-          <button onClick={handleClickPlus} className={styles.button}>
-            <img src={person} className={styles.icon} alt="Person Icon" />
-          </button>
-        </div>
-      </div>
-      <a className={styles.viewMore} href={props}>
+      ))}
+      <a className={styles.viewMore} href={allStudentsLink}>
         View More
       </a>
+      </>
     </div>
   );
 };
