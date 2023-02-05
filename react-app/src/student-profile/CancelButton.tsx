f34bbe871a9dfd974c340fb043c61c9f2e908b09
Wenjie <wwei4949@gmail.com>
2023-02-05 11:25:44 -0500

    added cancel functionality

--- react-app/src/student-profile/CancelButton.tsx
+++ react-app/src/student-profile/CancelButton.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./StudentCreation.module.css";
 
 export default function CancelButton() {
   return (
-    <button className={styles.cancelButton} type="submit">
-      Cancel
-    </button>
+    <NavLink to="/search" end>
+      <button className={styles.cancelButton} type="submit">
+        Cancel
+      </button>
+    </NavLink>
   );
 }
